f817857592b551ec3fab65fe0cd63d5d84d41ba7
Kontses <your.email@example.com>
2025-04-04 01:19:11 +0300

    Add CORS headers and API configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
   swcMinify: true,
   images: {
     unoptimized: true
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
   }
 }
 
@@ -18,7 +32,15 @@ export default withPWA({
     runtimeCaching: [
       {
         urlPattern: /^https:\/\/qr-code-.*\.vercel\.app\/api\/(users\/login|users\/me)/,
-        handler: 'NetworkOnly'
+        handler: 'NetworkOnly',
+        options: {
+          backgroundSync: {
+            name: 'api-queue',
+            options: {
+              maxRetentionTime: 24 * 60 // Retry for max of 24 hours (specified in minutes)
+            }
+          }
+        }
       },
       {
         urlPattern: /^https:\/\/qr-code-.*\.vercel\.app\/api\/.*/,
